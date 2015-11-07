0981bd9fd5fbd2c88c3585e3a0c11ab662fc743a
Vincent Perot <vincent@perot.me>
2015-11-07 20:45:40 +0100

    Fix text and form

--- php/html_form_send.php
+++ php/html_form_send.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "vincent.perot@gmail.com";
+    $email_to = "bob@gmail.com";
      
     $email_subject = "[Website] New message from contact section";
      
